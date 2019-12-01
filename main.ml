80702fd2bba79caa2d2be48f160b05f0f18e943a
Basile Henry <bjm.henry@gmail.com>
2019-12-01 22:36:19 +0100

    Day 1

--- main.ml
+++ main.ml
@@ -1,1 +1,11 @@
-let () = print_endline "Hello, World!"
+let () =
+    let day = int_of_string Sys.argv.(1) in
+    let part = int_of_string Sys.argv.(2) in
+    let data =
+        Std.input_list (open_in (Printf.sprintf "data/day%02d/input" day)) in
+    let answer =
+        match (day, part) with
+        | (1, 1) -> Day01.problem1 data
+        | (1, 2) -> Day01.problem2 data
+        | _ -> failwith "Not implemented" in
+    Printf.printf "%d\n" answer
