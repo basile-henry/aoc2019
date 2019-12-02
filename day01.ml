04248a5622b2c6777337cc7857be45630d04de25
Basile Henry <bjm.henry@gmail.com>
2019-12-02 13:05:38 +0100

    Refactor day 1

--- day01.ml
+++ day01.ml
@@ -1,12 +1,16 @@
 let simple_fuel mass = Int.div mass 3 - 2
-let sumWith f = List.fold_left (fun acc x -> acc + f x) 0
+let sumWith f data =
+    List.fold_left
+      (fun acc x -> acc + f x)
+      0
+      (List.filter_map int_of_string_opt (String.split_on_char '\n' data))
 
 let problem1 =
-    sumWith (fun line -> simple_fuel (int_of_string line))
+    sumWith simple_fuel
 
 let fuel mass = max 0 (simple_fuel mass)
 let rec fix_fuel x =
     if x == 0 then 0 else x + fix_fuel (fuel x)
 
 let problem2 =
-    sumWith (fun line -> fix_fuel (fuel (int_of_string line)))
+    sumWith (fun mass -> fix_fuel (fuel mass))
--- main.ml
+++ main.ml
@@ -2,10 +2,11 @@ let () =
     let day = int_of_string Sys.argv.(1) in
     let part = int_of_string Sys.argv.(2) in
     let data =
-        Std.input_list (open_in (Printf.sprintf "data/day%02d/input" day)) in
+        Std.input_all (open_in (Printf.sprintf "data/day%02d/input" day)) in
     let answer =
         match (day, part) with
         | (1, 1) -> Day01.problem1 data
         | (1, 2) -> Day01.problem2 data
+        | (2, 1) -> Day02.problem1 data
         | _ -> failwith "Not implemented" in
     Printf.printf "%d\n" answer
