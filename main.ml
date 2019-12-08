94f2ff7eb394ab51c1917ddf82c19873eb26797e
Basile Henry <bjm.henry@gmail.com>
2019-12-08 12:28:20 +0000

    Re-organise data

--- main.ml
+++ main.ml
@@ -2,7 +2,7 @@ let () =
     let day = int_of_string Sys.argv.(1) in
     let part = int_of_string Sys.argv.(2) in
     let data () =
-        Std.input_all (open_in (Printf.sprintf "data/day%02d/input" day)) in
+        Std.input_all (open_in (Printf.sprintf "data/day%02d.txt" day)) in
     let answer =
         match (day, part) with
         | (1, 1) -> Day01.problem1 (data ())
@@ -13,7 +13,11 @@ let () =
         | (3, 2) -> Day03.problem2 (data ())
         | (4, 1) -> Day04.problem1 ()
         | (4, 2) -> Day04.problem2 ()
+        | (5, 1) -> Day05.problem1 (data ())
+        | (5, 2) -> Day05.problem2 (data ())
         | (6, 1) -> Day06.problem1 (data ())
         | (6, 2) -> Day06.problem2 (data ())
+        | (8, 1) -> Day08.problem1 (data ())
+        | (8, 2) -> Day08.problem2 (data ())
         | _ -> failwith "Not implemented" in
     Printf.printf "%d\n" answer
